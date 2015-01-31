f36ebb032e9f165411a3f6a45997e8f2c64fa14a
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2015-01-31 03:17:21 +0100

    showing how to get strongly a typed Optional<> result from tryGet()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,9 @@
+import com.tinkerpop.gremlin.structure.Vertex;
+import com.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
+import com.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+
+import java.util.Date;
+
 /**
  * @author Daniel Kuppitz (http://gremlin.guru)
  */
@@ -5,5 +11,21 @@ public class App {
 
     public static void main(final String[] args) {
 
+        final TinkerGraph g = TinkerFactory.createModern();
+        final Vertex marko = g.V(1).next();
+        final Vertex vadas = g.V(2).next();
+        final Model model = new Model();
+
+        marko.property("date", System.currentTimeMillis());
+
+        marko.<Long>values("date").tryNext().map(Date::new).ifPresent(model::setDate);
+        vadas.<Long>values("date").tryNext().map(Date::new).ifPresent(model::setDate);
+    }
+
+    static class Model {
+
+        public void setDate(final Date date) {
+            System.out.println("Setting date in model: " + date);
+        }
     }
 }
