ecff48f35a9a1da3bfbc13c3432d5412ff73ce89
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2016-03-04 00:04:58 +0100

    just another example for the mailing list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,9 @@
-import com.tinkerpop.blueprints.Graph;
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
-import com.tinkerpop.gremlin.java.GremlinPipeline;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.eq;
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.select;
+import static org.apache.tinkerpop.gremlin.structure.T.id;
 
 /**
  * @author Daniel Kuppitz (http://gremlin.guru)
@@ -13,32 +12,14 @@ public class App {
 
     public static void main(final String[] args) {
 
-        final Graph g = new TinkerGraph();
+        final GraphTraversalSource g = TinkerGraph.open().traversal();
 
-        for (int i = 1; i <= 4; i++) {
-            g.addVertex("S" + i).setProperty("type", "sender");
-            g.addVertex("R" + i).setProperty("type", "receiver");
-        }
+        g.addV("person").property(id, 1).property("name", "bob").as("a").
+                addV("person").property(id, 2).property("name", "robert").property("nick", "bob").as("b").
+                addE("knows").from("a").to("b").iterate();
 
-        g.getVertex("S1").addEdge("tx", g.getVertex("R1"));
-        g.getVertex("S2").addEdge("tx", g.getVertex("R2"));
-        g.getVertex("S2").addEdge("tx", g.getVertex("R3"));
-        g.getVertex("S3").addEdge("tx", g.getVertex("R4"));
-        g.getVertex("S4").addEdge("tx", g.getVertex("R4"));
-
-        System.out.println("\n== one sender to many receivers ==");
-        new GremlinPipeline<>(g).V().has("type", "sender").as("s").cast(Vertex.class).
-                filter(v -> new GremlinPipeline<>(v).outE("tx").range(0, 2).count() > 1).
-                transform(v -> new GremlinPipeline<>(v).outE("tx").inV().path().cast(List.class).
-                        transform(p -> p.subList(1, 3)).toList()).as("r").
-                select(Arrays.asList("s", "r")).forEachRemaining(System.out::println);
-
-
-        System.out.println("\n== many senders to one receiver ==");
-        new GremlinPipeline<>(g).V().has("type", "receiver").as("r").cast(Vertex.class).
-                filter(v -> new GremlinPipeline<>(v).inE("tx").range(0, 2).count() > 1).
-                transform(v -> new GremlinPipeline<>(v).inE("tx").outV().path().cast(List.class).
-                        transform(p -> p.subList(1, 3)).toList()).as("r").
-                select(Arrays.asList("s", "r")).forEachRemaining(System.out::println);
+        g.V(1).as("t").out().as("r").
+                filter(select("t").by("name").as("tn").select("r").by("nick").as("rn").where("tn", eq("rn"))).
+                forEachRemaining(System.out::println);
     }
 }
