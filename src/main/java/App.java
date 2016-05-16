ec2b6bef05da5e2c7faf9ca69c483130b65e28dc
Daniel Kuppitz <daniel_kuppitz@hotmail.com>
2016-05-16 13:33:05 +0200

    mailing list example

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
+
+import java.util.Random;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.P.eq;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.lt;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.neq;
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.as;
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.not;
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.select;
 import static org.apache.tinkerpop.gremlin.structure.T.id;
 
@@ -12,14 +18,25 @@ public class App {
 
     public static void main(final String[] args) {
 
-        final GraphTraversalSource g = TinkerGraph.open().traversal();
+        final GraphTraversalSource g = TinkerFactory.createModern().traversal();
+        final Random rand = new Random();
 
-        g.addV("person").property(id, 1).property("name", "bob").as("a").
-                addV("person").property(id, 2).property("name", "robert").property("nick", "bob").as("b").
-                addE("knows").from("a").to("b").iterate();
-
-        g.V(1).as("t").out().as("r").
-                filter(select("t").by("name").as("tn").select("r").by("nick").as("rn").where("tn", eq("rn"))).
+        System.out.println("\n-- extend vertex properties");
+        g.V().sideEffect(t -> t.get().property("id", rand.nextInt(5))).valueMap().
                 forEachRemaining(System.out::println);
+
+        System.out.println("\n-- 1st iteration");
+        g.V().as("a").aggregate("x").select("x").unfold().where(neq("a")).as("b").
+                filter(select("a", "b").by(id).where("a", lt("b"))).
+                filter(select("a", "b").by("id").where("a", eq("b"))).
+                where(not(as("a").out("related").as("b"))).
+                addOutE("a", "related", "b").forEachRemaining(System.out::println);
+
+        System.out.println("\n-- 2nd iteration");
+        g.V().as("a").aggregate("x").select("x").unfold().where(neq("a")).as("b").
+                filter(select("a", "b").by(id).where("a", lt("b"))).
+                filter(select("a", "b").by("id").where("a", eq("b"))).
+                where(not(as("a").out("related").as("b"))).
+                addOutE("a", "related", "b").forEachRemaining(System.out::println);
     }
 }
